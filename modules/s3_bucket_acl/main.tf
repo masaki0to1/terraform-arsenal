198cf9dcd55f618018c2fe5a83b254b31ca89645
Masaki Ishimoto <masaki0to1>
2024-11-18 18:49:58 +0900

    refactor: Split resources into separate files

--- modules/s3_bucket_acl/main.tf
+++ modules/s3_bucket_acl/main.tf
@@ -2,10 +2,3 @@ resource "aws_s3_bucket_acl" "this" {
   bucket = var.bucket_id
   acl    = var.acl
 }
-
-resource "aws_s3_bucket_ownership_controls" "this" {
-  bucket = var.bucket_id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
